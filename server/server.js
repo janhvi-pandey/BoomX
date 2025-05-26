809105980231d34a7c55b063c7c645dd99fdf0c6
Janhvi Pandey <shivipandey993@gmail.com>
2025-05-26 23:10:21 +0530

    Login & Register routes added

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const connectDB = require('./config/db');
+const authRoutes = require('./routes/auth');
+
 
 const app = express();
 
@@ -13,6 +15,8 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+// Routes
+app.use('/auth', authRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
