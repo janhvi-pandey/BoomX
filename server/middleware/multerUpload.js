5a778723be80d1968c557db65c5c97528e5eafa0
Janhvi Pandey <shivipandey993@gmail.com>
2025-06-02 21:44:30 +0530

    multerupload file changed

--- server/middleware/multerUpload.js
+++ server/middleware/multerUpload.js
@@ -1,8 +1,15 @@
 const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+// It ensure uploads directory exists dynamically
+fs.mkdirSync(uploadDir, { recursive: true });
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
